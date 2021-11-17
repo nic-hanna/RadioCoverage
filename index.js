a031427b67957d216f49d54723b6888ad06c88bb
teddy9264 <nhanna70@gmail.com>
2021-11-18 00:39:48 +1100

    dev and prod

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require('express')
+const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const e = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express()
 const port = 3000
 
+var environment = process.env.NODE_ENV
+var isDevelopment = environment === 'development'
+
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'))
 app.use(express.json());
+if (isDevelopment) {
+    const key = fs.readFileSync('key.pem');
+    const cert = fs.readFileSync('cert.pem');
 
-const key = fs.readFileSync('key.pem');
-const cert = fs.readFileSync('cert.pem');
-
-const server = https.createServer({key: key, cert: cert }, app);
-server.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+    const server = https.createServer({key: key, cert: cert }, app);
+    server.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+    })
+} else {
+    const httpServer = http.createServer(app).listen(4000);
+}
 
 let db = new sqlite3.Database('./database.db', (err) => {
     if (err) {
