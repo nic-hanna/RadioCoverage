df5db04de789d117ce5639f132fddd8e6a1d8519
teddy9264 <nhanna70@gmail.com>
2021-11-17 19:48:43 +1100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const https = require('https');
+const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'))
+app.use(express.json());
+
+const key = fs.readFileSync('key.pem');
+const cert = fs.readFileSync('cert.pem');
+
+const server = https.createServer({key: key, cert: cert }, app);
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+let db = new sqlite3.Database('./database.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to database.');
+  });
+
+app.get('/', (req, res) => {
+    db.all(`SELECT testID, lat, long, location, rssi FROM tests`, [], (err, rows) => {
+        res.render('index', {"tests":rows})    
+    });
+  
+})
+
+app.post('/newLocation', (req, res) => {
+    console.log(req.body)
+    var location = req.body.location;
+    var rssi = req.body.rssi;
+    var lat = req.body.lat;
+    var long = req.body.long;
+    if (location & rssi & lat & long){
+        db.run(`INSERT INTO tests(location, rssi, lat, long)
+            VALUES(?, ?, ?, ?)`, 
+            [location, rssi, lat, long])
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(400)
+    }
+})
